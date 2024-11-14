0f3c63b5364bef57c43c0776de0caf8fd9461dbc
Wesley R. S. <130418293+Wesley00s@users.noreply.github.com>
2024-11-14 16:41:51 -0300

    Update main.py

--- Exercicio2/main.py
+++ Exercicio2/main.py
@@ -1,1 +1,37 @@
+import csv
 
+def carregaBanco(file_name):
+    with open(file_name, mode='r', newline='', encoding='utf-8') as f:
+        conteudo = csv.DictReader(f, delimiter=';')
+        dados = list(conteudo)
+    return dados
+
+
+def mediaRenda(file_name):
+    dados = carregaBanco(file_name)
+    soma = 0
+    count = 0
+    for i in dados:
+        soma += float(i["Renda"])
+        count += 1
+    if count > 0:
+        print(f'A média das rendas é: {soma / count}')
+    else:
+        print('Não há dados válidos de renda.')
+
+
+def mediaIdade(file_name):
+    dados = carregaBanco(file_name)
+    soma = 0
+    count = 0
+    for i in dados:
+        soma += float(i["Idade"])
+        count += 1
+    if count > 0:
+        print(f'A média das idades é: {soma / count}')
+    else:
+        print('Não há dados válidos de idade.')
+
+
+mediaRenda("dados.csv")
+mediaIdade("dados.csv")
